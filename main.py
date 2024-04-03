285b28dcf1205a611a4fb53672f0ca332743a7fc
Jonathan <jongroberg@gmail.com>
2024-04-02 20:34:27 -0400

    added bad input handling for user choice and capacity

--- main.py
+++ main.py
@@ -1,38 +1,57 @@
 main_menu = ("Padudex Main Menu"
-             "-----------------"
-             "1. List Pakuri"
-             "2. Show Pakuri"
-             "3. Add Pakuri"
-             "4. Evolve Pakuri"
-             "5. Sort Pakuri"
-             "6. Exit"
+             "\n-----------------"
+             "\n1. List Pakuri"
+             "\n2. Show Pakuri"
+             "\n3. Add Pakuri"
+             "\n4. Evolve Pakuri"
+             "\n5. Sort Pakuri"
+             "\n6. Exit"
              "")
 
 
 def handle_user_choice(choice: str) -> None:
     if choice == "1":
         pass
-    if choice == "2":
+    elif choice == "2":
         pass
-    if choice == "3":
+    elif choice == "3":
         pass
-    if choice == "4":
+    elif choice == "4":
         pass
-    if choice == "5":
+    elif choice == "5":
         pass
-    if choice == "6":
+    elif choice == "6":
         exit()
+    else:
+        print("Unrecognized menu selection!")
+
+
+def prompt_capacity() -> int:
+    capacity = 0
+    while capacity == 0:
+        user_input = input("Enter max capacity of the Pakudex ")
+
+        try:
+            if int(user_input) > 0:
+                capacity = user_input
+                print(f"The pakudex can hold {capacity} species of Pakuri.\n")
+            else:
+                print("Please enter a valid size")
+        except ValueError:
+            print("Please enter a valid size")
+            continue
+
+    return capacity
 
 
 def main() -> None:
     print("Welcome to Pakudex: Tracker Extraordinaire! ")
-    pakudex_capacity = input("Enter max capacity of the Pakudex")
-    print(f"The pakudex can hold {pakudex_capacity} species of Pakuri.")
+    pakudex_capacity = prompt_capacity()
 
     # run the menu loop
     while True:
         print(main_menu)
-        choice = input("What would you like to do? ")
+        choice = input("\nWhat would you like to do? ")
         handle_user_choice(choice)
 
 
